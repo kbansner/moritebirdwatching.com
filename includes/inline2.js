6eb0da699c1195bb2341b87b74abfdedd6e4e937
Kai Bansner <kai@bansner.com>
2018-11-19 20:41:15 -0800

    replace transition with animate to fix event issues

--- includes/inline2.js
+++ includes/inline2.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
   var currSlide = 0;
-  var $slides = $('.slide');
+  var $slider = $('.slider');
+  var $slides = $slider.children('.slide');
+  var nextSlide = function(){
+    console.log('nextSlide', $slides.length, currSlide);
+    if (currSlide < $slides.length -1) {
+      $slider[0].scrollBy({left: $slider.width(), behavior: 'smooth'})
+      bindNext(currSlide + 1);
+    }
+    else {
+      $slider[0].scrollTo({left: 0, behavior: 'smooth'});
+    }
+  }
+  var bindNext = function(i){
+    $slides.eq(i).find('.slide-background').one('animationend', nextSlide);
+  }
+  bindNext(0);
   $slides.viewportChecker({
     scrollHorizontal: true,
     classToAdd: 'on',
     classToAddForFullView: '',
     repeat: true,
-    scrollBox: '.slider',
+    scrollBox: $slider[0],
     callbackFunction: function($elem, action){
       if (action==='add') {
         $elem.next().removeClass('lazyload')
           .next().removeClass('lazyload');
-      // animationend auto advance
       } else {
         currSlide = $slides.index($slides.filter('.on'));
-        console.log(currSlide);
       }
     }
   });
